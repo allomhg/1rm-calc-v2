f5bc379378cc837082b25b0fa9ff2feffd981792
allomhg <allomhg@gmail.com>
2024-09-23 21:46:05 +0100

    Created basic functionality with table

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,7 +1,27 @@
-const Form = () => {
+import Table from "./Table";
+
+const Form: React.FC = () => {
     return (
         <form>
-            <p>This is a form</p>
+            <div>
+                <div>
+                    <select id="units" name="units">
+                        <option value="metric">Metric</option>
+                        <option value="value">Imperial</option>
+                    </select>
+                </div>
+                <div>
+                    <label htmlFor="weight">Weight:</label>
+                    <input type="number" id="weight" name="weight" min="1" placeholder="Enter weight" />
+                </div>
+                <div>
+                    <label htmlFor="reps">Repetitions:</label>
+                    <input type="number" id="reps" name="reps" min="1" placeholder="Enter number of reps" />
+                </div>
+                <button>Calculate</button>
+            </div>
+            <button>Reset</button>
+            <Table/>
         </form>
     );
 }
---
+++ src/components/Table.tsx
@@ -0,0 +1,48 @@
+interface TableRow {
+    id: number;
+    percentage: number;
+    weight: number;
+    reps: number;
+}
+
+function generateRows ( ) {
+    return Array.from({length: 10 }, (v, i) => (
+        {
+            id: i + 1,
+            percentage: 100,
+            weight: 100 - (i + 10),
+            reps: 1 + i,
+        }
+    ));
+}
+
+const Table: React.FC = () => {
+    const rows: TableRow[] = generateRows();
+
+    return (
+        <section>
+            <table>
+                {/* TABLE HEADER */}
+                <thead>
+                    <tr>
+                        <td>1 RM %</td>
+                        <td>Weight</td>
+                        <td>Reps</td>
+                    </tr>
+                </thead>
+                {/* TABLE BODY */}
+                <tbody>
+                    {rows.map((row) => (
+                        <tr key={row.id}>
+                            <td>{row.percentage}</td>
+                            <td>{row.weight}</td>
+                            <td>{row.reps}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </section>
+    );
+}
+
+export default Table;
